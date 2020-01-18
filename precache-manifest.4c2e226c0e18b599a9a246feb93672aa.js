37e6bcf95009b876c7779a74cb6a95d6f29e6caf
Pei <stevenkaoo@gmail.com>
2020-01-19 02:50:15 +1100

    Updates

---
+++ precache-manifest.4c2e226c0e18b599a9a246feb93672aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07e6065657d7bf4a9d86025f3dcea5a4",
+    "url": "/react-todo/index.html"
+  },
+  {
+    "revision": "9c5e4e7f86cde33a5086",
+    "url": "/react-todo/static/css/main.1e984c33.chunk.css"
+  },
+  {
+    "revision": "9bc9d53b7e93e48f3e03",
+    "url": "/react-todo/static/js/2.10413e1b.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/react-todo/static/js/2.10413e1b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "9c5e4e7f86cde33a5086",
+    "url": "/react-todo/static/js/main.e120bcaa.chunk.js"
+  },
+  {
+    "revision": "aa6e551850d3baf5f93e",
+    "url": "/react-todo/static/js/runtime-main.ccd8bf63.js"
+  }
+]);
